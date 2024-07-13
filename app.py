119b9e58641d3092d52e76f643317c00e0b2578b
Fernando-Lopez-86 <ferdinandolopez@hotmail.com>
2024-07-12 21:16:29 -0300

    Creacion funciones de preprocesamiento de texto: limpieza, tokenizacion y normalizacion previo a crear los embeddings

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import streamlit as st
 import os
-from PyPDF2 import PdfReader
 import docx
 import pandas as pd
+import spacy
+import fitz  # PyMuPDF
+import re
+import openpyxl
 
+# Cargar el modelo de spaCy para español 
+# Descargar el modelo de lenguaje español
+# !python -m spacy download es_core_news_sm - VER INSTALACION EN SCRIPT O ALTERNATIVA
+nlp = spacy.load('es_core_news_sm')
 
 def extract_text_pdf(file_path):
-    with open(file_path, 'rb') as f:
-        pdf = PdfReader(f)
-        text = ''
-        for page_num in range(len(pdf.pages)):
-            page = pdf.pages[page_num]
-            text += page.extract_text()
+    text = ''
+    with fitz.open(file_path) as pdf:
+        for page_num in range(len(pdf)):
+            page = pdf.load_page(page_num)
+            text += page.get_text("text")
     return text
 
 def extract_text_word(file_path):
@@ -21,9 +27,28 @@ def extract_text_word(file_path):
 
 def extract_text_excel(file_path):
     xls = pd.read_excel(file_path)
-    text = xls.to_string(index=False)
+    # Convertir DataFrame a string, y despues limpia el texto ignorando valores NaN que se repiten mucho
+    text = xls.fillna('').astype(str).apply(lambda x: ' '.join(x), axis=1).str.cat(sep=' ')
     return text
 
+# Eliminar caracteres no deseados y normalizar espacios
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text)
+    text = text.strip()
+    return text
+
+# Creacion de tokens
+def create_tokens(text):
+    doc = nlp(text)
+    tokens_normalizados = []
+
+    for token in doc:
+        if not token.is_stop and not token.is_punct and token.text.strip():
+            tokens_normalizados.append(token.lemma_)
+
+    print("Texto:", text.strip())
+    print("Tokens normalizados:", tokens_normalizados)
+    return tokens_normalizados
 
 # Carpeta donde almacenamos los documentos de la empresa
 uploaded_files = "data/files/"   # Quitar ruta hardcodeada - Revisar
@@ -43,10 +68,17 @@ if uploaded_file:
     # Procesar el archivo pdf subido para crear los embeddings - Revisar tipo de archivos OJO!!
     if uploaded_file.type == "application/pdf":
         text = extract_text_pdf(file_path)
-    elif uploaded_file.type == "application/doc":
+    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
         text = extract_text_word(file_path)
-    elif uploaded_file.type == "application/xls":
+    elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
         text = extract_text_excel(file_path)
+    else:
+        st.sidebar.error("Tipo de archivo no soportado.")
+        text = None
+
+    if text:
+        text = clean_text(text)
+        tokens = create_tokens(text)
 
     st.sidebar.success("Archivo subido y procesado correctamente.")
 
